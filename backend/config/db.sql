fd92b3d3b36d7dc99a69adbf3b71b810a2d80a1d
gio-palagashvili <girgi1201@gmail.com>
2023-03-10 00:05:59 +0400

    databse updated prob completed

--- backend/config/db.sql
+++ backend/config/db.sql
@@ -9,8 +9,25 @@ CREATE TABLE users_tbl (
 );
 
 CREATE table invoices_tbl (
+    invoice_index serial PRIMARY KEY,
+    invoice_id VARCHAR(255) NOT NULL,
+    billing_address VARCHAR(255) NOT NULL,
+    billing_city VARCHAR(255) NOT NULL,
+    billing_country VARCHAR(255) NOT NULL,
+    due_date date not null,
+    initial_date date not null,
+    invoice_total VARCHAR(255) NOT NULL,
     user_id int,
     text VARCHAR(255) NOT NULL,
     CONSTRAINT fk_users_tbl FOREIGN key (user_id) REFERENCES users_tbl (user_id) ON
     DELETE CASCADE
 );
+
+CREATE table invoice_item_tbl (
+    invoice_index int,
+    qty VARCHAR(255),
+    price VARCHAR(255),
+    total VARCHAR(255),
+    CONSTRAINT fk_invoices_tbl FOREIGN key (invoice_index) REFERENCES invoices_tbl (invoice_index) ON
+    DELETE CASCADE
+)
